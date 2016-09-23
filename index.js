3e9e231760c8373a9ceb642a10f881393f0e9b07
Tony Mai <tonymaibox@yahoo.com>
2016-09-23 08:46:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+// Source code
+const { foo, bar } = myLib;
+ 
+// Gets transformed by the plugin to:
+var _myLib = myLib;
+var foo = _myLib.foo;
+var bar = _myLib.bar;
